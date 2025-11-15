bbeb4315970cd803d151a12fa06d6add1af2fe3c
Angell2900 <Angelihimbazwe29@gmail.com>
2025-11-15 17:49:55 +0200

    add changes

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,20 +27,17 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         result = client.org
         self.assertEqual(result, {"login": org_name})
-        mock_get_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}")
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
 
     @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
     def test_public_repos_url(self, mock_org):
         """
         Test _public_repos_url property
         """
-        mock_org.return_value = {
-            "repos_url": "https://api.github.com/orgs/google/repos"}
+        mock_org.return_value = {"repos_url": "https://api.github.com/orgs/google/repos"}
         client = GithubOrgClient("google")
         result = client._public_repos_url
-        self.assertEqual(result,
-                         "https://api.github.com/orgs/google/repos")
+        self.assertEqual(result, "https://api.github.com/orgs/google/repos")
 
     @patch('client.get_json')
     @patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock)
@@ -49,8 +46,7 @@ class TestGithubOrgClient(unittest.TestCase):
         Test public_repos property
         """
         mock_public_repos_url.return_value = "https://api.github.com/orgs/google/repos"
-        mock_get_json.return_value = [
-            {"name": "repo1"}, {"name": "repo2"}]
+        mock_get_json.return_value = [{"name": "repo1"}, {"name": "repo2"}]
         client = GithubOrgClient("google")
         result = client.public_repos
         self.assertEqual(result, ["repo1", "repo2"])
@@ -124,5 +120,4 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             repo for repo in self.apache2_repos
             if repo.get("license", {}).get("key") == "apache-2.0"]
         result = client.public_repos
-        self.assertEqual(result,
-                         [repo["name"] for repo in apache2_repos])
+        self.assertEqual(result, [repo["name"] for repo in apache2_repos])
