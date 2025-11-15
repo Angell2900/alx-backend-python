2c73517555fb4a9583315309ec4f03b61674ee87
Angell2900 <Angelihimbazwe29@gmail.com>
2025-11-15 21:13:29 +0200

    Fix urls.py order and add api-auth for ALX checks

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('chats.urls')),
+    path('api/', include('chats.urls')),           # app routes first
+    path('api-auth/', include('rest_framework.urls')),  # DRF login/logout last
 ]
+
