bcd6544b6ca781cf5715af869a615dcc4032057e
Angell2900 <Angelihimbazwe29@gmail.com>
2025-11-15 21:15:14 +0200

    Fix urls.py for ALX api-auth check

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),           # app routes first
-    path('api-auth/', include('rest_framework.urls')),  # DRF login/logout last
+    path('api/', include('chats.urls')),  # app routes first
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),  # DRF login/logout
 ]
 
