a7855ebcd69585a4b48ac351b7b25ffa7b72e9f7
Angell2900 <Angelihimbazwe29@gmail.com>
2025-11-15 20:25:22 +0200

    Messaging App API project

---
+++ chats/views.py
@@ -0,0 +1,37 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    # create a new conversation with participants
+    def create(self, request, *args, **kwargs):
+        participant_ids = request.data.get("participants", [])
+
+        conversation = Conversation.objects.create()
+        conversation.participants.set(participant_ids)
+
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        conversation_id = request.data.get("conversation")
+        message_body = request.data.get("message_body")
+        sender = request.user  # assumes authentication enabled
+
+        message = Message.objects.create(
+            conversation_id=conversation_id,
+            sender=sender,
+            message_body=message_body
+        )
+
+        return Response(MessageSerializer(message).data, status=status.HTTP_201_CREATED)
