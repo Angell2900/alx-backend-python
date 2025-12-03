5a1502c59bb5fad91c63b20bfcce98a73cc62952
Angell2900 <Angelihimbazwe29@gmail.com>
2025-12-03 17:06:49 +0300

    add notifaction feature

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,26 +1,23 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from .models import Message, MessageHistory
-from django.utils import timezone
 
 @receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, **kwargs):
     if not instance.pk:
         # New message, nothing to log
         return
-    
+
     try:
-        old_instance = Message.objects.get(pk=instance.pk)
+        old_message = Message.objects.get(pk=instance.pk)
     except Message.DoesNotExist:
         return
-    
-    if old_instance.content != instance.content:
-        # Save old content in history
+
+    if old_message.content != instance.content:
+        instance.edited = True  # mark as edited
+        # Save old content to history
         MessageHistory.objects.create(
-            message=instance,
-            old_content=old_instance.content,
-            edited_at=timezone.now(),
-            edited_by=instance.edited_by  # make sure your view sets this
+            message=old_message,
+            old_content=old_message.content,
+            edited_by=instance.edited_by
         )
-        # Update the edited timestamp
-        instance.edited_at = timezone.now()
