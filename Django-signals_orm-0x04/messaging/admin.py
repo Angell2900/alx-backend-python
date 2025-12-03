a227a419fa14308e4f7c5fdeda11d1eb5fba3fbd
Angell2900 <Angelihimbazwe29@gmail.com>
2025-12-03 17:18:32 +0300

    changes made to models.py file for the message class

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -3,7 +3,7 @@ from .models import Message, MessageHistory
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('content', 'edited_by', 'edited_at', 'created_at')
+    list_display = ('content', 'edited_by', 'edited_at', 'timestamp', 'sender', 'receiver', 'edited')
 
 @admin.register(MessageHistory)
 class MessageHistoryAdmin(admin.ModelAdmin):
