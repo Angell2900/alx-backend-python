74635ec440aee763c22c0cd8f511d9478b5bb619
Angell2900 <Angelihimbazwe29@gmail.com>
2025-12-03 17:25:46 +0300

    basic view cache

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.contrib.auth.decorators import login_required
+from .models import Message 
+@login_required
+def delete_user(request):
+    user = request.user
+    user.delete()  # Deletes the user
+    return redirect('home')  # Redirect to home page after deletion
 
-# Create your views here.
+@cache_page(60)  # 60-second cache
+def conversation_messages(request, conversation_id):
+    messages = Message.objects.filter(conversation_id=conversation_id).order_by('timestamp')
+    return render(request, 'messaging/conversation.html', {'messages': messages})
